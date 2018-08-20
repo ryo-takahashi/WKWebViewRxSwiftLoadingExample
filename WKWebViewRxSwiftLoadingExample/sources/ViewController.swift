dec75973f8ecc13c5ba51c2796f8639a03d53402
ryo-takahashi <ryo.takahashi.work@gmail.com>
2018-08-20 22:54:44 +0900

    add present button on ViewController

---
+++ WKWebViewRxSwiftLoadingExample/sources/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var presentObserverLoadingWebViewControllerButton: UIButton!
+    @IBOutlet weak var presentRxLoadingWebViewControllerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewController()
+    }
+    
+    private func setupViewController() {
+        
+    }
+}
